2b9fc55e60cab6043d2bab15872e8cfd16c0fc4a
irshadnazree <inazree370@gmail.com>
2025-11-13 09:17:18 +0800

    Refactor App component layout to display recent quotations and invoices
    - Removed ThemeToggle and Button components from the main layout.
    - Introduced sections for "Recent Quotations" and "Recent Invoices" with horizontal scrolling cards as placeholders.
    - Enhanced overall layout with improved spacing and structure for better user experience.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,32 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
-import { ThemeToggle } from "@/components/shared/theme-toggle";
-import { Button } from "@/components/ui/button";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <div className="absolute top-4 right-4">
-        <ThemeToggle />
+    <div className="flex flex-col items-center justify-center gap-8">
+      <div className="flex w-full flex-col items-start gap-4">
+        <h1 className="text-3xl">Recent Quotations</h1>
+        <div className="flex h-scrollbar w-full gap-12 overflow-x-auto pb-2">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <div
+              className="h-75 w-125 shrink-0 bg-text/20"
+              key={`card-${index}${1}`}
+            />
+          ))}
+        </div>
       </div>
-      <Link to="/quotation">
-        <Button>Quotation</Button>
-      </Link>
+      <div className="flex w-full flex-col items-start gap-4 pb-2">
+        <h1 className="text-3xl">Recent Invoices</h1>
+        <div className="flex h-scrollbar w-full gap-12 overflow-x-auto pb-2">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <div
+              className="h-75 w-125 shrink-0 bg-text/20"
+              key={`card-${index}${1}`}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
