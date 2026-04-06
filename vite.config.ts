5852a416a69b4df14b3704f02dae0b83ee9feace
irshadnazree <inazree370@gmail.com>
2026-04-07 00:01:57 +0800

    chore: add Oxfmt configuration and update dependencies
    - Introduced .oxfmtrc.json for formatting configuration with Prettier and Tailwind CSS support.
    - Updated package.json and pnpm-lock.yaml to reflect dependency version changes, including @cloudflare/vite-plugin, tailwindcss, and others for improved compatibility and features.
    - Added vitest.config.ts for testing configuration with Vitest.
    - Refactored vite.config.ts to streamline plugin usage and enable TypeScript path resolution.

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,19 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
+    devtools(),
     tailwindcss(),
     tanstackStart(),
-    viteReact({
-      babel: {
-        plugins: ["babel-plugin-react-compiler"],
-      },
-    }),
+    viteReact(),
   ],
+	resolve: {
+		tsconfigPaths: true
+	}
 });
 
 export default config;
